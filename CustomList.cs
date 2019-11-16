400ce2b1a083e7be4be239503c9ad6ed213c0fd7
hamptoam <ameliabedelia94@gmail.com>
2019-11-16 11:57:38 -0600

    Fixed mistakes in prior unit tests and completing more before writing index and zip methods.

---
+++ CustomList.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CustomListProject
+{
+    public class CustomList<T> : IEnumerable<T>
+    {
+        public int count { get; set; }
+        private T[] items;
+        int removeAt;
+        int i;
+
+        public T this[int i]
+        {
+            get => items[i];
+
+            set => items[i] = value;
+        }
+
+        public interface ICount
+        {
+            void Count();
+        }
+
+       
+        public int Capacity
+        {
+            get
+            {
+                return items.Length; 
+            }
+        }
+        public CustomList()
+        {
+            count = 0;
+            items = new T[Capacity];
+        }
+
+        public void Add(T item)
+        {
+
+            if (count >= Capacity)
+            {
+                Capacity *= 2;
+                T[] arrayToResize = new T[Capacity];
+
+                for (int i = 0; i < count; i++)
+                {
+                    arrayToResize[i] = items[i];
+                }
+                // point "items" to "arrayToResize" X
+                items = arrayToResize;
+              //  Objects = arrayToResize;
+
+            }
+
+            // add our item to the next open spot  in the "items" (count?)
+            count++;
+            items[count - 1] = item;
+            objects[count - 1] = Object;
+
+        }
+
+        public void Remove(T item)
+        {
+
+            if (count == 0)
+            {
+                return;
+            }
+            else
+            {
+                T[] arrayToShrink = items;
+
+                for (int i = Capacity; i > 0; i--)
+                {
+                    if (arrayToShrink.Length > i)
+                    {
+
+
+
+                    }
+
+                }
+                items = arrayToShrink;
+            }
+
+            // add our item to the next open spot  in the "items" (count?)
+            count--;
+            items[count - 1] = item;
+            //objects[count - 1] = object;
+        }
+        //Removing int from list but not sure if right int, find way to move integers over in the capacity
+
+
+       // T[]  = new T[];
+
+
+        public void Zip(CustomList<T> listToAdd)
+        {
+            
+            if (items.Length <= 0 || listToAdd.count <= 0)
+            {
+                return;
+            }
+            else
+            {
+
+
+
+            }
+
+           //T[] oldlist = new T[count];
+
+           // var itemsAndObjects = items.Add(objects, (first, second) => first + " " + second);
+        }
+
+        public void toString()
+        {
+
+
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+           for(int i = 0; i < count; i++)
+           {
+                yield return items[i];
+           }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
+
+
+
+    
---
+++ UnitTest1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomListProject;
+namespace CustomListClassTest
+{
+    [TestClass]
+    public class CustomList
+    {
+
+        [TestMethod]
+        public void Add_AddToEmptyList_ItemShouldGoToIndexZero()
+        {
+            CustomList<int> test = new CustomList<int>();
+            int expected = 8;
+            int actual;
+
+            test.Add(8);
+
+            actual = test[0];
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Add_AddToEmptyList_CountShouldIncreaseToOne()
+        {
+            CustomList<int> test = new CustomList<int>();
+            int expected = 1;
+            int actual;
+
+            test.Add(8);
+
+            actual = test.count;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public static void Add_AddingItemToFullArray_ItemStillAccepted()
+        {
+            CustomList<int> test = new CustomList<int>();
+            int expected = 9;
+            int actual;
+          
+            test.Add(1);
+            test.Add(1);
+            test.Add(1);
+            test.Add(1);
+            test.Add(1);
+            test.Add(1);
+            test.Add(1);
+            test.Add(1);
+            test.Add(1);
+
+            actual = test.Count;
+
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public static void Add_AddToPopulatedList_IndexShouldGoToCount()
+        {
+
+            CustomList<int> test = new CustomList<int>();
+
+            int expected = 2;
+            int actual;
+
+            test.Add(1);
+            test.Add(1);
+
+            actual = test.count;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public static void Remove_RemoveFromList_CountShouldReduce()
+        {
+            CustomList<int> test = new CustomList<int>();
+
+            int expected = 3;
+            int actual;
+
+
+            test.Add(1);
+            test.Add(1);
+            test.Add(1);
+            test.Add(1);
+            test.Remove(1);
+
+            actual = test. count;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public static void Remove_RemoveFromPopulatedList_IndexShouldShiftWhenSomethingRemoved()
+        {
+            CustomList<int> test = new CustomList<int> { 0, 1, 2, 3 };
+
+            int expected = 3;
+            int actual;
+
+            test.Remove(2);
+            actual = test[2];
+
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public static void Remove_RemoveFromList_IndexShouldGoTo0()
+        {
+
+            CustomList<int> test = new CustomList<int>();
+            int expected = 0;
+            int actual;
+
+            test.Add(1);
+            test.Add(1);
+            test.Remove(1);
+            test.Remove(1);
+
+            actual = test[0];
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public static void Remove_RemoveFromEmptyList_CountRemain0()
+        {
+            CustomList<int> test = new CustomList<int>();
+
+            int expected = 0;
+            int actual; 
+
+            test.Add(1);
+            test.Remove(1);
+            test.Remove(1); 
+
+            actual = test[0];
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public static void Indexer_FindAnObjectInAList_GetRightObject()
+        {
+            CustomList<int> test = new CustomList<int>();
+
+            int expected = 8;
+            int actual;
+
+
+            test.Add(1);
+            test.Add(2);
+            test.Add(3);
+            test.Add(4);
+            test.Add(5);
+            test.Add(6);
+            test.Add(7);
+            test.Add(8);
+
+            actual = test.Count;
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        
+
+        [TestMethod]
+        public static void Zip_TieTwoArrays_ItemsWereZippedSuccessfully()
+        {
+            CustomList<int> test = new CustomList<int>();
+            test.Add(1);
+            test.Add(2);
+            test.Add(3);
+            test.Add(4);
+
+
+            CustomList<int> test2 = new CustomList<int>();
+            test2.Add(5);
+            test2.Add(6);
+            test2.Add(7);
+            test2.Add(8);
+
+            test.Zip(test2);
+
+            actual = 
+        }
+
+    }      
+}
